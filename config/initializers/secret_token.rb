5e1dfca40bd86f03125e417d5298f9675f350021
Benjamin Wheeler-Chong <btwcnoob@users.noreply.github.com>
2014-04-01 21:59:34 -0400

    add rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Boltweet::Application.config.secret_key_base = 'bca8ebea578a048b709688b5b58b1198a915907d133916f715ee05973150f54b3ec618ad4990bbfea307f2c98a9125ce184d0996fa6e5f61712a677ec811e1fe'
