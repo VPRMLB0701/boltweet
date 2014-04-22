de730e5232b94eb4c524daeccc170e7d49f737c8
Benjamin Wheeler-Chong <btwcnoob@users.noreply.github.com>
2014-04-22 19:58:58 -0400

    remove twitter_creds file

--- config/initializers/twitter_credentials.rb
+++
@@ -1,8 +0,0 @@
- require 'twitter'
-
-Twitter.configure do |config|
-  config.consumer_key = 'oEfomtv0zPPg9kquaByC3tnup'
-  config.consumer_secret = 'xyw95jseFkIKGLFkxxIBbNuWlOQ7TYBMsF2APNiaShMwJWGf60'
-  config.oauth_token = 'your_oauth_token_here '
-  config.oauth_token_secret = 'your_oauth_token_secret_here'
-end 
