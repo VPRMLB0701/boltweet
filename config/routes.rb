f23cc46f9c6408c4341ef715320e22e69ded6bb5
Jeff <jfzx6r@users.noreply.github.com>
2014-05-13 20:52:17 -0400

    setup site controller for root (index) page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Boltweet::Application.routes.draw do
+
+  root "site#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
